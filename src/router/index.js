5d88aee9cc2c8b2124b9592a9ef90887ded7fd9e
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-12-20 23:40:41 +0200

    umopomrochitelnie changes

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><h3>Login</h3></div>
+</template>
